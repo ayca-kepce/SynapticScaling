c6851a9197882eae9c004b0a506d3ac7ac0962d2
ayca-kepce <aycakepce@gmail.com>
2022-10-06 16:45:15 +0200

    Add function to create connections with given probabilty and strength

---
+++ util.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def connection_probability(weights, probability, strength):
+    """
+    Function to randomly create the connections between two populations
+    :param weights: A matrix filled with 1s in the size of number of post- and pre-synaptic neurons
+    :param probability: The connectivity probability
+    :param strength: The strength of the connections
+    :return: 
+    """
+    np.random.seed(1)
+    num_elements = weights.shape[1] * weights.shape[0]
+    indices = np.random.choice(num_elements, replace=False, size=int(num_elements * probability))
+    weights[np.unravel_index(indices, weights.shape)] = 0
+    return weights*strength
