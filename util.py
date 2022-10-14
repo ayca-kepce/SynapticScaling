6bf2baa25d39fcf6b6342afcee68947c879f8c80
ayca-kepce <aycakepce@gmail.com>
2022-10-14 16:17:49 +0200

    Fix connection probability

--- util.py
+++ util.py
@@ -10,6 +10,8 @@ def connection_probability(weights, probability, strength):
     """
     np.random.seed(1)
     num_elements = weights.shape[1] * weights.shape[0]
-    indices = np.random.choice(num_elements, replace=False, size=int(num_elements * probability))
+    indices = np.random.choice(num_elements, replace=False, size=int(num_elements * (1-probability)))
     weights[np.unravel_index(indices, weights.shape)] = 0
+    """print("probability is ", probability)
+    print("calculated as ", np.count_nonzero(weights)/num_elements)"""
     return weights*strength
