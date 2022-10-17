a390e47dec0664c9e8e5e34b272f0955e572736d
ayca-kepce <aycakepce@gmail.com>
2022-10-17 13:18:51 +0200

    Add saving and printing functions

--- util.py
+++ util.py
@@ -1,4 +1,8 @@
 import numpy as np
+from matplotlib import pyplot as plt
+import pickle
+from numba import cuda, jit
+
 
 def connection_probability(weights, probability, strength):
     """
@@ -15,3 +19,27 @@ def connection_probability(weights, probability, strength):
     """print("probability is ", probability)
     print("calculated as ", np.count_nonzero(weights)/num_elements)"""
     return weights*strength
+
+def save_params(path, config_id, params):
+    pickle.dump(params, open(path + config_id + '.pkl', 'wb'))
+
+def save_plot(name, rE, rP, rS, t):
+    plt.plot(t[:], rE[:], 'r', label='PC, saturates at '+ str(np.round(np.mean(rE[-80:]),3)) + 'Hz')
+    plt.plot(t[:], rP[:], 'g', label='PV, saturates at '+ str(np.round(np.mean(rP[-80:]),3)) + 'Hz')
+    plt.plot(t[:], rS[:], 'b', label='SST, saturates at '+ str(np.round(np.mean(rS[-80:]),3)) + 'Hz')
+    plt.legend(loc='best')
+    plt.xlabel('time [s]')
+    plt.ylabel('Firing rates [Hz]')
+    plt.grid()
+    plt.savefig(name)
+    plt.clf()
+
+def show_plot(rE, rP, rS, t):
+    plt.plot(t[:], rE[:], 'r', label='PC, saturates at '+ str(np.round(np.mean(rE[-80:]),3)) + 'Hz')
+    plt.plot(t[:], rP[:], 'g', label='PV, saturates at '+ str(np.round(np.mean(rP[-80:]),3)) + 'Hz')
+    plt.plot(t[:], rS[:], 'b', label='SST, saturates at '+ str(np.round(np.mean(rS[-80:]),3)) + 'Hz')
+    plt.legend(loc='best')
+    plt.xlabel('time [s]')
+    plt.ylabel('Firing rates [Hz]')
+    plt.grid()
+    plt.show()
