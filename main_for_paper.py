1a8134bd03731f7cbf7dada48ed686e0d04aaf37
ayca-kepce <aycakepce@gmail.com>
2024-02-06 18:24:40 +0100

    Fix minor bug in calling an item in Figure 6

--- main_for_paper.py
+++ main_for_paper.py
@@ -15,7 +15,7 @@ dir_plot = directory + r'\figures\\'
 
 ### Hebbian learning, the third factor in the three-factor Hebbian learning, and adaptive set-point are active in all figures
 hebbian_flag, three_factor_flag, adaptive_set_point_flag= 1, 1, 1
-
+"""
 
 
 ### Plotting Figure 2
@@ -106,7 +106,7 @@ analyze_model(48, flags_list, dir_data = dir_data, dir_plot = dir_plot + r'figur
 plot_testing_at_regular_intervals(flags_list, dir_data = dir_data, dir_plot = dir_plot + r'figure5\\',
                                   run_simulation=0, save_results =1, plot_results=1)
 
-
+"""
 
 ### Plotting Figure 6
 # Turning on only the flag of E-to-E scaling
@@ -140,9 +140,9 @@ plot_testing_at_regular_intervals(flags_list[0:2], dir_data = dir_data, dir_plot
                                   run_simulation=0, save_results =1, plot_results=1)
 
 # Plotting margins are different for only SST-to-E scaling on case, thus the related flag is set to True
-analyze_model(24, flags_list[2], dir_data = dir_data, dir_plot = dir_plot + r'figure6\\',
+analyze_model(24, flags_list[2:], dir_data = dir_data, dir_plot = dir_plot + r'figure6\\',
               flag_only_S_on=True, run_simulation=0, save_results=1, plot_results=1)
-analyze_model(48, flags_list[2], dir_data = dir_data, dir_plot = dir_plot + r'figure6\\',
+analyze_model(48, flags_list[2:], dir_data = dir_data, dir_plot = dir_plot + r'figure6\\',
               flag_only_S_on=True, run_simulation=0, save_results=1, plot_results=1)
-plot_testing_at_regular_intervals(flags_list[2], dir_data = dir_data, dir_plot = dir_plot + r'figure6\\',
+plot_testing_at_regular_intervals(flags_list[2:], dir_data = dir_data, dir_plot = dir_plot + r'figure6\\',
                                   flag_only_S_on=True, run_simulation=0, save_results =1, plot_results=1)
